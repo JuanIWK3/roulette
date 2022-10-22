bdf57c181a7ac6c26ba787878119b6b4fda8ebc0
juaniwk3 <juaniwk3@gmail.com>
2022-10-22 18:24:55 -0300

    style

---
+++ src/App.tsx
@@ -0,0 +1,116 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.scss";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <div className="roulette">
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+        <div className="number"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
